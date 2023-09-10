f826f4dbb2dab8666313cfb4ffaffee295befa00
Aldin <alhuric89@gmail.com>
2023-09-10 22:56:17 +0200

    finish project

--- app.js
+++ app.js
@@ -1,82 +1,120 @@
-// Get a reference to your game container and the insect elements
-const gameContainer = document.querySelector('.game');
-const insect = document.querySelector('.insect');
-const scoreEl = document.querySelector('.score');
-const time = document.querySelector('.time');
-const play = document.getElementById('play');
-const start = document.getElementById('start');
-const settings = document.getElementById('settings');
-const game = document.getElementById('game');
-let score = +scoreEl.innerHTML;
-// Select the "fly" card by its ID
-const flyCard = document.getElementById('fly');
+document.addEventListener('DOMContentLoaded', () => {
+	// Get references to your game elements
+	const play = document.getElementById('play');
+	const start = document.getElementById('start');
+	const settings = document.getElementById('settings');
+	const game = document.getElementById('game');
+	const scoreEl = document.querySelector('.score');
+	const playField = document.querySelector('.playField');
 
-// Select the "mosquito" card by its ID
-const mosquitoCard = document.getElementById('mosquito');
+	// Select the "fly" card by its ID
+	const flyCard = document.getElementById('fly');
 
-// Select the "spider" card by its ID
-const spiderCard = document.getElementById('spider');
+	// Select the "mosquito" card by its ID
+	const mosquitoCard = document.getElementById('mosquito');
 
-// Select the "roach" card by its ID
-const roachCard = document.getElementById('roach');
-// Initialize a variable to store the selected insect
-let selectedInsect = null;
+	// Select the "spider" card by its ID
+	const spiderCard = document.getElementById('spider');
 
-// Select the "fly" card by its ID and add a click event listener
-play.addEventListener('click', () => {
-	start.classList.toggle('active');
-	settings.classList.toggle('active');
+	// Select the "roach" card by its ID
+	const roachCard = document.getElementById('roach');
+
+	// Initialize variables
+	let score = 0;
+	const maxScore = 20; // Define the maximum score to win the game
+	let gameWon = false; // Flag to track whether the game has been won
+	let selectedInsect = null;
+
+	// Function to create a new insect and add it to the game
+	function createInsect() {
+		if (!gameWon && score < maxScore) {
+			const newInsect = document.createElement('div');
+			newInsect.classList.add('insect');
+
+			// Set the position and rotation randomly
+			const maxX = playField.clientWidth - newInsect.clientWidth;
+			const maxY = playField.clientHeight - newInsect.clientHeight;
+			console.log(maxY);
+			const randomX = Math.floor(Math.random() * maxX);
+			const randomY = Math.floor(Math.random() * maxY);
+			const randomRotation = Math.floor(Math.random() * 360);
+
+			newInsect.style.left = `${randomX}px`;
+			newInsect.style.top = `${randomY}px`;
+			newInsect.style.transform = `rotate(${randomRotation}deg)`;
+
+			// Set the image source
+			const image = document.createElement('img');
+			image.src = `img/${selectedInsect}.jpg`;
+			newInsect.appendChild(image);
+			function handleInsectClick() {
+				if (!gameWon) {
+					newInsect.remove();
+					score++;
+					scoreEl.innerHTML = `Score: ${score}`;
+					placeInsectRandomly();
+					placeInsectRandomly();
+					if (score >= maxScore) {
+						gameWon = true; // Set the gameWon flag to true
+						// Remove the click event listener using the same function
+						newInsect.removeEventListener('click', handleInsectClick);
+
+						// Handle game win condition
+						const message = document.createElement('div');
+						message.classList.add('message');
+						message.innerHTML = `<h1>Congratulations! You won the game!</h1>`;
+						playField.appendChild(message);
+
+						game.style.backgroundColor = `rgba(6, 131, 240, 0.3)`;
+					}
+				}
+			}
+			// Add click event listener to the new insect
+			newInsect.addEventListener('click', handleInsectClick);
+
+			// Append the new insect to the playField
+			playField.appendChild(newInsect);
+		}
+	}
+
+	// Function to set the insect's position and rotation randomly
+	function placeInsectRandomly() {
+		if (!gameWon && score < maxScore) {
+			createInsect();
+		}
+	}
+
+	// Event listeners to start the game
+	play.addEventListener('click', () => {
+		start.classList.toggle('active');
+		settings.classList.toggle('active');
+	});
+
+	flyCard.addEventListener('click', () => {
+		selectedInsect = 'fly';
+		settings.classList.toggle('active');
+		game.classList.toggle('active');
+		placeInsectRandomly();
+	});
+
+	mosquitoCard.addEventListener('click', () => {
+		selectedInsect = 'mosquito';
+		settings.classList.toggle('active');
+		game.classList.toggle('active');
+		placeInsectRandomly();
+	});
+
+	spiderCard.addEventListener('click', () => {
+		selectedInsect = 'spider';
+		settings.classList.toggle('active');
+		game.classList.toggle('active');
+		placeInsectRandomly();
+	});
+
+	roachCard.addEventListener('click', () => {
+		selectedInsect = 'roach';
+		settings.classList.toggle('active');
+		game.classList.toggle('active');
+		placeInsectRandomly();
+	});
 });
-flyCard.addEventListener('click', () => {
-	selectedInsect = 'fly';
-	settings.classList.toggle('active');
-	game.classList.toggle('active');
-});
-
-// Select the "mosquito" card by its ID and add a click event listener
-
-mosquitoCard.addEventListener('click', () => {
-	selectedInsect = 'mosquito';
-	console.log('Selected insect: ' + selectedInsect);
-	settings.classList.toggle('active');
-	game.classList.toggle('active');
-});
-
-// Select the "spider" card by its ID and add a click event listener
-
-spiderCard.addEventListener('click', () => {
-	selectedInsect = 'spider';
-	console.log('Selected insect: ' + selectedInsect);
-	settings.classList.toggle('active');
-	game.classList.toggle('active');
-});
-
-// Select the "roach" card by its ID and add a click event listener
-
-roachCard.addEventListener('click', () => {
-	selectedInsect = 'roach';
-	console.log('Selected insect: ' + selectedInsect);
-	settings.classList.toggle('active');
-	game.classList.toggle('active');
-});
-
-// Function to set the insect's position and rotation randomly within the game container
-function placeInsectRandomly() {
-	// Calculate random coordinates within the game container's dimensions
-	const maxX = gameContainer.clientWidth - insect.clientWidth;
-	const maxY = gameContainer.clientHeight - insect.clientHeight;
-
-	const randomX = Math.floor(Math.random() * maxX);
-	const randomY = Math.floor(Math.random() * maxY);
-
-	// Calculate a random rotation angle between 0 and 360 degrees
-	const randomRotation = Math.floor(Math.random() * 360);
-
-	// Set the insect's position and rotation
-	insect.style.left = `${randomX}px`;
-	insect.style.top = `${randomY}px`;
-	insect.style.transform = `rotate(${randomRotation}deg)`;
-}
-
-// Call the function to initially position and rotate the insect
-placeInsectRandomly();
